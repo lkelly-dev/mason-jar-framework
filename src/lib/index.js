b6df7a779753f3ab1eacf834aa794cda34b0ea4a
Liam Kelly <me@liamckelly.com>
2019-01-22 11:09:37 -0500

    Removed assets

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,3 @@
-import './components/assets/fonts/feather/feather.css';
-import './components/assets/libs/highlight.js/styles/vs2015.css';
-import './components/assets/libs/quill/dist/quill.core.css';
-import './components/assets/libs/select2/dist/css/select2.min.css';
-import './components/assets/libs/flatpickr/dist/flatpickr.min.css';
-
-import './components/assets/css/theme.min.css';
-
-// import { Button } from './components/core/Button';
-// import * as core from './components/coreFile';
-
 export * from './components/core/Alert';
 export * from './components/core/Avatar';
 export * from './components/core/Badge';
@@ -29,4 +18,3 @@ export * from './components/core/Pagination';
 export * from './components/core/Progress';
 export * from './components/core/Table';
 
-// export default core;
